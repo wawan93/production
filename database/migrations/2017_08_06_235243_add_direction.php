1fe88ec88fd038de6509eef56d80bbf2f68bfbde
Vladimir Kochergin <root@wawan.pro>
2017-08-07 03:02:40 +0300

    direction

---
+++ database/migrations/2017_08_06_235243_add_direction.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDirection extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('polygraphy_invoices', function (Blueprint $table) {
+            $table->enum('direction', ['invoice', 'payment'])->default('invoice');
+            $table->dropIndex('invoices_user_id_order_id_uindex');
+            $table->unique(['direction', 'order_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('polygraphy_invoices', function (Blueprint $table) {
+            //
+        });
+    }
+}
