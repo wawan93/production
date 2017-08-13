fe64e0a0f1f078b230a09f5055133d23a540da2e
Vladimir Kochergin <root@wawan.pro>
2017-08-13 19:23:44 +0300

    фильтры в флоу склада

--- app/Http/Controllers/WarehouseController.php
+++ app/Http/Controllers/WarehouseController.php
@@ -11,6 +11,45 @@ class WarehouseController extends Controller
     {
         $order = Order::warehouse();
 
+        $filter = (array)$request->get('filter');
+
+        if (@$filter['code_name']) {
+            $order->where('code_name', 'like', '%' . $filter['code_name'] . '%');
+        }
+
+        if (@$filter['status']) {
+            $order->where('status', $filter['status']);
+        }
+
+        if (@$filter['manufacturer']) {
+            $order->where('manufacturer', $filter['manufacturer']);
+        }
+
+        if (@$filter['ship_date']) {
+            $order->where('ship_date', $filter['ship_date']);
+        }
+
+        if (@$filter['received']) {
+            $order->where('received', 1);
+        }
+
+        if (@$filter['sorted']) {
+            $order->where('sorted', 1);
+        }
+
+        if (@$filter['delivery']) {
+            $order->where('delivery', 1);
+        }
+
+        if (@$filter['docs']) {
+            $order->where('docs', 1);
+        }
+
+        if (@$filter['docs_in_shtab']) {
+            $order->where('docs_in_shtab', 1);
+        }
+
+
         return view('warehouse.index', ['order'=> $order->paginate(1000), 'filter' => [], 'count' => $order->count()]);
     }
 }
