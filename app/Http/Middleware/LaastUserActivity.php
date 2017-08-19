9b81b0ac63c2264b9141bf5cb55efa59bea9a004
Vladimir Kochergin <root@wawan.pro>
2017-08-19 17:57:55 +0300

    list buildings

---
+++ app/Http/Middleware/LaastUserActivity.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class LaastUserActivity
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        Auth::user();
+        return $next($request);
+    }
+}
