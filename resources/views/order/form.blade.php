1457aa21278367feda28ab6700b630d6ccf9a5aa
Vladimir Kochergin <root@wawan.pro>
2017-08-30 12:02:59 +0300

    fix форматы в карточке заказа

--- resources/views/order/form.blade.php
+++ resources/views/order/form.blade.php
@@ -45,9 +45,7 @@
 <div class="form-group {{ $errors->has('polygraphy_format') ? 'has-error' : ''}}">
     {!! Form::label('polygraphy_format', 'Формат', ['class' => 'col-md-4 control-label']) !!}
     <div class="col-md-6">
-        {!! Form::select('polygraphy_format', [
-            'A3' => 'A3', 'A4' => 'A4', 'np' => 'np', 'offset' => 'offset', 'А3' => 'А3 (колбаса)', 'А4х3' => 'колбаса'
-            ], null, ['class' => 'form-control']) !!}
+        {!! Form::select('polygraphy_format', \App\PolygraphyType::all()->pluck('format', 'format')->all(), null, ['class' => 'form-control']) !!}
         {!! $errors->first('polygraphy_format', '<p class="help-block">:message</p>') !!}
     </div>
 </div>
