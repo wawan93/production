0678cf1ee6f466e02cbac147b7a2c564a2399326
Vladimir Kochergin <root@wawan.pro>
2017-09-03 17:40:09 +0300

    whitespaces

--- app/User.php
+++ app/User.php
@@ -8,48 +8,49 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-	use Notifiable;
+    use Notifiable;
 
-	protected $table = 'main_users';
+    protected $table = 'main_users';
 
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array
-	 */
-	protected $fillable = [
-		'name', 'email', 'password',
-	];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
 
-	/**
-	 * The attributes that should be hidden for arrays.
-	 *
-	 * @var array
-	 */
-	protected $hidden = [
-		'password',
-	];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
 
     protected $guarded = ['password', 'salt'];
 
     public $timestamps = false;
 
-	public static function managers()
-	{
-		$users = static::where('extra_class', 'like', '%c_orders_manager%')->get();
+    public static function managers()
+    {
 
-		$result = [
-			0 => 'нет'
-		];
-		foreach ($users as $user) {
-			$result[$user->id] = $user->surname . ' ' . $user->name;
-		}
+        $users = static::where('extra_class', 'like', '%c_orders_manager%')->get();
 
-		return $result;
-	}
+        $result = [
+            0 => 'нет'
+        ];
+        foreach ($users as $user) {
+            $result[$user->id] = $user->surname . ' ' . $user->name;
+        }
 
-	public function getElectionAttribute() {
-		$region_names = $this->hasOne(RegionNames::class, 'region_name', 'region_name')->first();
-		return $region_names->election;
-	}
+        return $result;
+    }
+
+    public function getElectionAttribute() {
+        $region_names = $this->hasOne(RegionNames::class, 'region_name', 'region_name')->first();
+        return $region_names->election;
+    }
 }
